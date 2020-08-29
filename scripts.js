b21401f5269282150a4cc97c1f516dc313fcea76
rita gupta <ritagupta1014@gmail.com>
2020-08-29 11:56:30 +0545

    bootstrap jquery part 2, one single button is added to pause and play the slide

--- scripts.js
+++ scripts.js
@@ -1,3 +1,47 @@
 $(document).ready(function(){
     $("[data-toggle ='tooltip']").tooltip();
+
+
+
+    $("#mycarousel").carousel({interval:1000});
+    // $("#carousel-pause").click(function(){
+    //     $("#mycarousel").carousel('pause');
+
+    // });
+    // $("#carousel-play").click(function(){
+    //     $("#mycarousel").carousel('cycle');
+        
+    // });
+
+
+    // using only one button to pause and play
+$("#carouselButton").click(function(){
+    if($("#carouselButton").children("span").hasClass('fa-pause')){
+        $("#mycarousel").carousel('pause');
+
+        $("#carouselButton").children("span").removeClass('fa-pause');
+        
+        $("#carouselButton").removeClass('btn-danger');
+
+        $("#carouselButton").children("span").addClass('fa-play');
+
+        $("#carouselButton").addClass('btn-success');
+
+     } else if ($("#carouselButton").children("span").hasClass('fa-play')){
+
+        $("#mycarousel").carousel('cycle');
+
+        $("#carouselButton").children("span").removeClass('fa-play');
+
+        $("#carouselButton").removeClass('btn-success');
+
+        $("#carouselButton").children("span").addClass('fa-pause');
+
+        $("#carouselButton").addClass('btn-danger');
+
+
+     }
 });
+   
+
+   });
